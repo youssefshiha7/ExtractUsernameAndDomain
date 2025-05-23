2fd912569267c31dfca93842113744f3551c6fc7
Yossef Shiha <Yossefshiha2001@gmail.com>
2025-05-23 18:43:54 +0300

    ADD all File to tracking

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Our Program");
+
+        System.out.print("Enter Your Email Address: ");
+        Scanner input = new Scanner(System.in);
+        String email = input.nextLine();
+
+        if(email.contains("@"))
+        {
+            String username = email.substring(0, email.indexOf("@"));
+            String domain = email.substring(email.indexOf("@") + 1);
+            System.out.println("Your Username is: " + username);
+            System.out.println("Your Domain is: " + domain);
+        }
+        else
+        {
+            System.out.println("Your Email Address is incorrect");
+        }
+
+
+    }
+}
