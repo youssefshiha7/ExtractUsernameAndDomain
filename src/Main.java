cb9dca008bdf3d27a7efc1269035dad602ea1920
Yossef Shiha <Yossefshiha2001@gmail.com>
2025-05-24 19:05:28 +0300

    Modifing While loop

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         System.out.print("Enter Your Email Address: ");
         Scanner input = new Scanner(System.in);
         String email = input.nextLine();
-        Boolean done = false;
+        boolean done = false;
 
-        while (done != true)
+        while (!done)
         {
             if(email.contains("@"))
             {
@@ -18,10 +18,13 @@ public class Main {
                 String domain = email.substring(email.indexOf("@") + 1);
                 System.out.println("Your Username is: " + username);
                 System.out.println("Your Domain is: " + domain);
+                done = true;
             }
             else
             {
                 System.out.println("Your Email Address is incorrect");
+                System.out.print("Enter Your Email Address: ");
+                email = input.nextLine();
             }
         }
 
